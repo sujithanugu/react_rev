6290aeaa5e691212290b7ccf98e013e969047f1d
sujithanugu <anugusujithreddy4444@gmail.com>
2024-04-11 01:03:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {id: "h1c"}, "I'm H1"),
+    React.createElement("h2", {}, "I'm H2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {id: "h1c"}, "I'm H1"),
+    React.createElement("h2", {}, "I'm H2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//console.log(root);
+root.render(heading);
