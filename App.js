0f67e8362a6ffcb7ba2e5f7f0aa1a7a6ed4f7beb
sujithanugu <anugusujithreddy4444@gmail.com>
2024-04-11 02:13:44 +0530

    THIRD commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {id: "h1c"}, "I'm H1"),
+    React.createElement("h1", { id: "h1c" }, "I'm H1"),
     React.createElement("h2", {}, "I'm H2"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {id: "h1c"}, "I'm H1"),
+    React.createElement("h1", { id: "h1c" }, "I'm H1"),
     React.createElement("h2", {}, "I'm H2"),
   ]),
 ]);
