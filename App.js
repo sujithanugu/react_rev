8e973b889d2170897ff2bce0c7eef6ac0d118c11
sujithanugu <anugusujithreddy4444@gmail.com>
2024-04-12 02:05:22 +0530

    fourth commit

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1c" }, "I'm H1"),
+    React.createElement("h1", { id: "h1c" }, "I'm #H-TAG"),
     React.createElement("h2", {}, "I'm H2"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-React Dev
+# React Dev
+
+# PARCEL
+- Dev Build
+- Local Server
+- HMR: Hot Module Replacement
+- File watching algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Copmpression
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking
+- Different Dev and Prod Bundles
