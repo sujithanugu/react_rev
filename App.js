cd304c3e0484ff4faf7c24a31c3b02dfa82583ba
sujithanugu <anugusujithreddy4444@gmail.com>
2024-04-17 01:18:29 +0530

    fifth commit

--- App.js
+++ App.js
@@ -3,7 +3,29 @@ import ReactDOM from "react-dom/client";
 
 // @ const heading = React.createElement("h1", {id: "heading"}, "Hello World!");
 //Using JSX
-const heading = <h1 id="heading">Hello World!</h1>;
-//console.log(heading); here heading is an object same as @
+// const heading = <h1>Hello World!</h1>
+// react element is converted to functional component
+const Heading = () => (
+  <h1 id="heading" className="head" tabIndex="5">
+    Hello World!
+  </h1>
+);
+
+//functional component is a normal js function
+const HeadingComponent = () => (
+  <div id="container">
+    <Heading/>  
+    <h1 id="heading" className="top">
+      Namaste React!!
+    </h1>
+  </div>
+);
+/*placing component(Heading) inside component(HeadingComponent) is called component composition*/
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
+
+
+
+
+
+
